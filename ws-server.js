3d82e39795861b0a9adcd092e33654faee784006
Mark Terpstra <markterp@cloud.com>
2015-02-04 09:19:02 -0500

    Support more than two ws connectoins, WIP

--- ws-server.js
+++ ws-server.js
@@ -1,24 +1,40 @@
 var ws = require("nodejs-websocket")
 
 
+var connections;
+
 var conn01 = null, 
     conn02 = null;
+
+function S4() {
+	    return (((1+Math.random())*0x10000)|0).toString(16).substring(1); 
+}
+ 
+function guid()
+{
+	guid = (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
+	return guid;
+}
+
+function onDisconnect(guid) 
+{
+	connections[guid].close();
+}
   
 // Scream server example: "hi" -> "HI!!!" 
 var server = ws.createServer(function (conn) {
 
-	console.log("New connection")
+	var newguid = guid();
+	console.log("New connection:", newguid)
 
-	if (conn01 == null) {
-		console.log ("Assigning connection to conn01");
-		conn01 = conn;
+	connections[guid] = conn;
 
-		conn01.on("text", function (str) {
-			console.log("Received on conn01, sending to conn02:" + str);
-			if (conn01 != null) {
-				conn02.sendText(str);
-			}
-		});
+	conn.on("text", function (str, guid) {
+		console.log("Received from " + guid + ": " + str);
+		if (conn01 != null) {
+			conn02.sendText(str);
+		}
+	});
 
 		conn01.on("close", function (code, reason) {
 			console.log("conn01 closed")
